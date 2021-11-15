87363e1a69f0b99eea0c11b595d2d8c51d52d12f
Prakash Tamang <89492512+PRATMG@users.noreply.github.com>
2021-11-15 10:13:40 -0600

    Update fighter.hpp

--- Assignment/P03B/fighter.hpp
+++ Assignment/P03B/fighter.hpp
@@ -5,28 +5,109 @@
 
 #include "weapon.hpp"
 #include "helpers.hpp"
-
+#include "dice.hpp"
 using namespace std;
 
 #pragma once
 
 
-class BaseFighter {
+class BaseFighter
+{
+protected:
     string name;
-    Weapon *weapon;
+    Weapon *weapon1;
+    int hp;
+    double rr;
 
 public:
-    BaseFighter() {
+    BaseFighter()
+    {
         name = "None";
-        weapon = new Weapon;
+        weapon1 = new Weapon;
     }
 
-    virtual double attack(){
-        return weapon->use();
+    virtual double attack()
+    {
+        return weapon1->use();
     }
 
-    friend ostream& operator<<(ostream& os, const BaseFighter& f) {
-        return os << "[" << f.name << " , " << *f.weapon << "]";
+    void takeDamage(int x)
+    {
+        hp -= x;
     }
 
+    bool alive()
+    {
+        return hp > 0;
+    }
+    int getHp()
+    {
+        return hp;
+    }
+
+    friend ostream &operator<<(ostream &os, const BaseFighter &f)
+    {
+        return os << "[" << f.name << " , " << *f.weapon1 << "]";
+    }
+};
+
+class Warrior : public BaseFighter
+{
+private:
+    Weapon *weapon2;
+
+public:
+    void attack(BaseFighter *&other)
+    {
+        int r = weapon2->use();
+        other->takeDamage(r);
+    }
+};
+
+class Wizard : public BaseFighter
+{
+private:
+    Weapon* weapon2;
+public:
+    void attack(BaseFighter *&other)
+    {
+        int r = weapon2->use();
+        other->takeDamage(r);
+    }
+};
+
+class Archer : public BaseFighter
+{
+private:
+    Weapon* weapon2;
+public:
+    void attack(BaseFighter *&other)
+    {
+        int r = weapon2->use();
+        other->takeDamage(r);
+    }
+};
+
+class Elf : public BaseFighter
+{
+private:
+    Weapon* weapon2;
+public:
+    void attack(BaseFighter *&other)
+    {
+        int r = weapon2->use();
+        other->takeDamage(r);
+    }
+};
+
+class DragonBorn : public BaseFighter
+{
+private:
+    Weapon* weapon2;
+public:
+    void attack(BaseFighter *&other)
+    {
+        int r = weapon2->use();
+        other->takeDamage(r);
+    }
 };
